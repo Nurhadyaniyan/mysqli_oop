7998def778a70a7d8f65792dc73bb13360771985
Nurhadyaniyan <nurhadyaniyan@gmail.com>
2018-04-09 10:57:43 +0700

    langkah ke 7 perintah escape agar tidak kena sql injection

--- index.php
+++ index.php
@@ -13,12 +13,16 @@
 		echo ' gagal konek ke mysqli ' . $mysqli->connect_error;
 	}
 
+
+	//7. Sebelum memasukan data ke database harus di escape agar tidak kena mysql injection
+	$nama = $mysqli->real_escape_string($_GET['user']);
+
 	//3. cara ketiga untuk memasukan data
 	// $sql = "INSERT INTO tutorial (murid, alamat) VALUES ('nurhadyan', 'jakarta timur')";
 
-	//6. Memasukan data lebih dari satu
-	$sql = "INSERT INTO tutorial (murid, alamat) VALUES ('saf', 'jakarta timur');";
-	$sql .= "INSERT INTO tutorial (murid, alamat) VALUES ('herni', 'jakarta timur')";
+	//5. Memasukan data lebih dari satu
+	// $sql = "INSERT INTO tutorial (murid, alamat) VALUES ('saf', 'jakarta timur');";
+	// $sql .= "INSERT INTO tutorial (murid, alamat) VALUES ('herni', 'jakarta timur')";
 
 	//4. Menguji apakah berhasil atau gagal memasukan database
 	// if ($mysqli->query($sql) == TRUE) {
@@ -27,12 +31,17 @@
 	// 	echo 'GAGAL';
 	// }
 
-	//6.. Cara menguji berhasil atau gagal dengan multi query
-	if ($mysqli->multi_query($sql) == TRUE) {
-		echo "BERHASIL";
-	}else{
-		echo 'GAGAL';
-	}
+	//6. Cara menguji berhasil atau gagal dengan multi query
+	// if ($mysqli->multi_query($sql) == TRUE) {
+	// 	echo "BERHASIL";
+	// }else{
+	// 	echo 'GAGAL';
+	// }
+
+	//escape-mysql injection
+	//prepare injection
+
+
 
 	//2. menutup koneksi terlebih dahulu
 	$mysqli->close();
