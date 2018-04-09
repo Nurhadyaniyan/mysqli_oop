a03cec7153dba9c12b877b4f0c798b3ad0652b64
Nurhadyaniyan <nurhadyaniyan@gmail.com>
2018-04-09 14:42:38 +0700

    select data dengan prepare statement

--- select.php
+++ select.php
@@ -11,13 +11,26 @@
     echo "gagal konek ke mysql" . $mysqli->connect_error;
   }
 
-  $query = "SELECT murid, alamat FROM tutorial";
-  $result = $mysqli->query($query);
+  // $query = "SELECT murid, alamat FROM tutorial";
+  // $result = $mysqli->query($query);
+  //
+  // if ($result->num_rows > 0) {
+  //   while ($row = $result->fetch_assoc()) {
+  //     echo $row['murid'] . ' ' . $row['alamat'] . '<br>';
+  //   }
+  // }
 
-  if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-      echo $row['murid'] . ' ' . $row['alamat'] . '<br>';
-    }
+  //select data dengan prepare statement
+  $murid_param = 'matraman';
+
+  $nama = $mysqli->prepare("SELECT murid, alamat FROM tutorial WHERE alamat=?");
+  $nama->bind_param('s', $murid_param);
+  $nama->execute();
+
+  $nama->bind_result($murid, $alamat);
+
+  while($nama->fetch()){
+    echo $murid . ' -  '. $alamat . '<br>';
   }
 
   $mysqli->close();
