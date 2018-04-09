b48f8b3e89d39879a7d6f25d91a807b06408bcd0
Nurhadyaniyan <nurhadyaniyan@gmail.com>
2018-04-09 11:22:21 +0700

    langkah ke 8 memasukan data dengan prepare statement

--- index.php
+++ index.php
@@ -13,15 +13,19 @@
 		echo ' gagal konek ke mysqli ' . $mysqli->connect_error;
 	}
 
-
+	//escape-mysql injection
 	//7. Sebelum memasukan data ke database harus di escape agar tidak kena mysql injection
-	$nama = $mysqli->real_escape_string($_GET['user']);
+	// $nama = $mysqli->real_escape_string($_GET['user']);
 
 	//3. cara ketiga untuk memasukan data
 	// $sql = "INSERT INTO tutorial (murid, alamat) VALUES ('nurhadyan', 'jakarta timur')";
 
+	//escape-mysql injection
+	//7. Sebelum memasukan data ke database harus di escape agar tidak kena mysql injection
+	// $nama = $mysqli->real_escape_string($_GET['user']);
+
 	//5. Memasukan data lebih dari satu
-	// $sql = "INSERT INTO tutorial (murid, alamat) VALUES ('saf', 'jakarta timur');";
+	// $sql = "INSERT INTO tutorial (murid, alamat) VALUES ('$nama', 'jakarta timur');";
 	// $sql .= "INSERT INTO tutorial (murid, alamat) VALUES ('herni', 'jakarta timur')";
 
 	//4. Menguji apakah berhasil atau gagal memasukan database
@@ -38,8 +42,21 @@
 	// 	echo 'GAGAL';
 	// }
 
-	//escape-mysql injection
-	//prepare injection
+	//8. prepare statement
+	//jika menggunakan prepare statement tidak perlu lagi memakai escape
+	$statement = $mysqli->prepare('INSERT INTO tutorial (murid, alamat) VALUES (?, ?)');
+	$statement->bind_param('ss', $nama, $alamat);
+
+	//9. Mengisi parameter + mengeksekusi
+	$nama = 'yuni';
+	$alamat = 'kayumanis';
+	$statement->execute();
+
+	$nama = 'iyan';
+	$alamat = 'utan kayu';
+	$statement->execute();
+
+
 
 
 
